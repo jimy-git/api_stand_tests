75ddb4a657b4b8ef1bf63a753b31df3cda33489c
José Luis Espinoza Saavedra <joseespinoza.saa@gmail.com>
2025-03-08 20:55:50 -0300

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://cnt-43917755-4f93-4ac9-aa27-d218a1360240.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,5 @@
+import configuration
+import requests
+import data
+
+def post_products_kits(products_ids):
